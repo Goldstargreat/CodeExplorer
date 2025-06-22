4c1006b89a7cfe7430b592a1b2ce2b9c9010b870
luckygoldstar <goldstargreat@gmail.com>
2025-06-22 16:21:58 +0900

    javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const headerNavs = [
+  {title: '웹 프로그래밍', link: '#about' },
+  {title: 'HTML', link: '#html' },
+  {title: 'CSS', link: '#css' },
+  {title: '커리큘럼', link: '#cirriculum' },
+  {title: '문의하기', link: '#contact' }
+];
+const $headerNavUlv = document.querySelector('.header__nav ul');
+
